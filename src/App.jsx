97ab4fe455e8a4c0d05a7a8fe056b0e4bbecd065
GreyHoldyngs <greyholdingsfx@gmail.com>
2025-10-17 00:44:12 +0100

    did not push all file

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Homepage from './components/Homepage.jsx/'
+
+function App() {
+
+  return (
+    <>
+      <Homepage />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Homepage.jsx
@@ -0,0 +1,10 @@
+
+
+const Homepage = () => {
+    return (
+        <h1 class="text-3xl font-bold underline">
+            Hello world!
+        </h1>
+    )
+}
+export default Homepage;
