587e34d200ca1cce1f23aacccefdf463a9209d12
GreyHoldyngs <greyholdingsfx@gmail.com>
2025-10-17 00:52:59 +0100

    created a folder component

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,10 +1,11 @@
-
+import React from 'react';
+import Navbar from './Navbar.jsx';
 
 const Homepage = () => {
     return (
-        <h1 class="text-3xl font-bold underline">
-            Hello world!
-        </h1>
+        <div>
+            <Navbar />
+        </div>
     )
 }
 export default Homepage;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Navbar = () => {
+    return(
+        <nav class="text-3xl font-bold underline">
+            this is navbar
+        </nav>
+    )
+}
+export default Navbar;
